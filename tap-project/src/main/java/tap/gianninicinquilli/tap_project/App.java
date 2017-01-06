1f532855e0fc213378d193004114c0aa444c37a1
Gabriele Giannini <gabrielegiannini2@gmail.com>
2017-01-06 14:54:49 +0100

    inizio Progetto

---
+++ tap-project/src/main/java/tap/gianninicinquilli/tap_project/App.java
@@ -0,0 +1,13 @@
+package tap.gianninicinquilli.tap_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
